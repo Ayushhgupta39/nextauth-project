b4aee8e88f372de3201883cb7e203248f68560f9
Ayush Gupta <ayushgupta3902@gmail.com>
2024-03-24 01:51:05 +0530

    feat: add landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import { AuroraBackgroundDemo } from "@/components/ui/Home/landing";
+import { AuroraBackground } from "@/components/ui/aurora-background";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black text-white">
-      Hello world!
+    <main className="flex min-h-screen flex-col w-full items-center justify-between bg-black text-white">
+      <AuroraBackground>
+        <AuroraBackgroundDemo />
+      </AuroraBackground>
     </main>
   );
 }
